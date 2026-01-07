cec7c5c50e10e2d1eee505e39dbea11f1eb70331
Vishwa Prakash <vishwaprakash@Vishwas-MacBook-Air.local>
2026-01-08 03:22:53 +0530

    changes to complaint components

--- frontend/src/components/ComplaintList.jsx
+++ frontend/src/components/ComplaintList.jsx
@@ -1,10 +1,20 @@
 
 
 
-const ComplaintList = () => {
+const ComplaintList = ({ complaintItem }) => {
     return (
-        <div>
-            <p>complaintlist**</p>
+        <div style={{boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)'}}>
+            <label>
+                <span style={{ padding: '10px 20px' }}>Title: </span>
+               <p style={{ padding: '10px 20px' }} >{complaintItem.title}</p>
+            <label>
+                <span style={{ padding: '10px 20px' }}>Description: </span>
+               <p style={{ padding: '10px 20px' }} >{complaintItem.description}</p>
+            </label>
+            <label></label>
+               <span style={{ padding: '10px 20px' }}>Category: </span>
+               <p style={{ padding: '10px 20px' }} >{complaintItem.category}</p>
+            </label>
         </div>
     )
 }
--- frontend/src/components/Complaints.jsx
+++ frontend/src/components/Complaints.jsx
@@ -1,16 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
 import API from '../utils/api';
 import ComplaintList from './ComplaintList';
 
 const Complaints = () => {
     
-    const[listofComplaints, setListofComplaints] = useState([]);
+    const [listofComplaints, setListofComplaints] = useState([]);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('other');
 
+    const addNewComplaint = async (e) => {
+        try
+        {
+            e.preventDefault();
+            const data = await API.post("/complaints", { title, description, category }); //change
+            alert("Complaint was Raised")
+        }
+        catch (err){
+            console.error(err);
+        }
+    }
     const fetchComplaints = async () => {
         try{
-            const data = await API.get(`url to fetch the complaints`);
-            setListofComplaints(data.info);
+            const response = await API.get("/complaints");
+            console.log('fetching details from server')
+            console.log(response.data);
+            setListofComplaints(response.data);
         }
         catch (err){
              console.error(err);
@@ -24,10 +39,23 @@ const Complaints = () => {
     return(
         <div>
             <p>This is complaints page</p>
-            <button className='complaintButton'>Add</button>
+            <form onSubmit={addNewComplaint}>
+                <label>Title</label>
+                <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required/>
+                <label>Description</label>
+                <input type="text" value={description} onChange={(e) => {setDescription(e.target.value)}} />
+                <label>category</label>
+                <select value={category} onChange={(e) => {setCategory(e.target.value)}}>
+                    <option value="Lift">Lift</option>
+                    <option value="Water">Water</option>
+                    <option value="Electricity">Electricity</option>
+                    <option value="Security">Security</option>
+                    <option value="Other">Other</option>
+                </select>
+                <button className='complaintButton' type='submit'>submit</button>
+            </form>            
             <p>List of complaints</p>
-            {listofComplaints.map(complaint => <ComplaintList key={complaint.id}
-          complaint={complaint}/>)}
+            {listofComplaints.map(eachComplaint => {return (<ComplaintList key={eachComplaint._id} complaintItem={eachComplaint}/>)})}
         </div>
     );
 };
